541209aa7f3150ab685cb1dcd7a420c2ae1f5382
lech957 <letsche@gmx.net>
2021-01-24 04:42:26 +0100

    Fixed typo

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text;
 using System.Security.Cryptography;
-using System.Collections.Generic;
 
 namespace PrintHashOfString
 {
@@ -18,21 +17,22 @@ namespace PrintHashOfString
         static string CleanString =>" ".PadRight(Console.BufferWidth,' ');
         static void Main(string[] args)
         {
-            
-
             hashAlgorithms hashAlg;
             Console.WriteLine("Hashalgorithms: MD5 (0), SHA1 (1), SHA256 (2), SHA384 (3), SHA512 (4)");
             var algC=Console.ReadKey(true);
+
             while (!Char.IsDigit(algC.KeyChar) || int.Parse(algC.KeyChar.ToString())<0 || int.Parse(algC.KeyChar.ToString())>4)
             {
                 Console.WriteLine($"{algC.KeyChar} is not valid. Enter a number between 0 and 5");
                 algC=Console.ReadKey(true);
             }
+
             hashAlg=(hashAlgorithms)int.Parse(algC.KeyChar.ToString());
             Console.WriteLine($"Using {hashAlg}");
             int rounds = 4;
             Console.Write("Enter number of rounds:");
             string rndStr=Console.ReadLine();
+
             while (! int.TryParse(rndStr, out rounds) || rounds<1){
                 Console.Write($"{rndStr} is invalid. Enter a valid number of rounds or e to exit:");
                 rndStr=Console.ReadLine();
@@ -40,21 +40,21 @@ namespace PrintHashOfString
                     return;
                 }
             }
-            Console.WriteLine($"Perfome {rounds} rounds for hashing.");
+
+            Console.WriteLine($"Performe {rounds} rounds for hashing.");
             Console.WriteLine("Enter text. Terminate program if string is empty.");
             string t =null;
             bool init=false;
             while (!string.IsNullOrEmpty((t = Console.ReadLine().Trim()))){
                 if (init)
-                    Console.WriteLine("Enter text. Terminate program if string is empty.");
-                
-                    
+                    Console.WriteLine("Enter text. Terminate program if string is empty.");              
                 byte[] orig= Encoding.UTF8.GetBytes(t);
                 using (HashAlgorithm hash = GetHashAlgorithm(hashAlg)){
                     byte[] current = orig;
                     int count =rounds;
                     string base64="";
                     byte[] h=null;
+
                     while (count >0){
                         h = hash.ComputeHash(current);
                         //Console.WriteLine(BitConverter.ToString(h));
@@ -62,6 +62,7 @@ namespace PrintHashOfString
                         current=Encoding.ASCII.GetBytes(base64);
                         count--;
                     }
+
                     ClearLastLines(init? 2 : 1);
                     Console.WriteLine("");
                     base64 =base64.TrimEnd('=');
@@ -69,9 +70,7 @@ namespace PrintHashOfString
                     Console.WriteLine($"{base64.Length}: {base64}");
                     Console.ReadKey();
                     ClearLastLines(2);
-
                     MoveLines(-1);
-
                     init=true;
                 }
             }
@@ -84,19 +83,15 @@ namespace PrintHashOfString
                 case hashAlgorithms.sha1: return SHA1.Create();
                 case hashAlgorithms.sha384: return SHA384.Create();
                 case hashAlgorithms.sha512: return SHA512.Create();
-  
-
             }
             return null;
         }
-
 
         static void ClearLastLines(int lines =1){
             Console.SetCursorPosition(0,Console.CursorTop-lines);
             for (int i=0;i<lines;i++)
             {
                 Console.WriteLine(CleanString);
-                //MoveLines(1);
             }
             Console.SetCursorPosition(0,Console.CursorTop-lines);
         }
@@ -105,7 +100,5 @@ namespace PrintHashOfString
         {
             Console.SetCursorPosition(0,Console.CursorTop+lines);
         }
-
     }
 }
-
